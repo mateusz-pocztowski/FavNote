3f78bf0693db7320710990033b2b5b7251287cbf
mateusz-pocztowski <55945204+mateusz-pocztowski@users.noreply.github.com>
2020-05-30 20:37:14 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn more
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
