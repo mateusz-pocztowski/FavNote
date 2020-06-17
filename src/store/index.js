7c21a0a9cb8576cfb8edefebd4cda0d1436217c9
mateusz-pocztowski <55945204+mateusz-pocztowski@users.noreply.github.com>
2020-06-17 17:40:51 +0200

    fixed localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -5,27 +5,27 @@ import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const persistedState = localStorage.getItem('reduxState')
-//   ? JSON.parse(localStorage.getItem('reduxState'))
-//   : {
-//       userJWT: null,
-//       userName: null,
-//       userID: null,
-//       isLoading: true,
-//       isSubmitting: false,
-//       error: {
-//         status: null,
-//         errorID: null,
-//       },
-//       modal: {
-//         itemType: null,
-//         itemID: null,
-//       },
-//     };
+const persistedState = localStorage.getItem('reduxState')
+  ? JSON.parse(localStorage.getItem('reduxState'))
+  : {
+      userJWT: null,
+      userName: null,
+      userID: null,
+      isLoading: true,
+      isSubmitting: false,
+      error: {
+        status: null,
+        errorID: null,
+      },
+      modal: {
+        itemType: null,
+        itemID: null,
+      },
+    };
 
 const store = createStore(
   rootReducer,
-  // persistedState,
+  persistedState,
   composeEnhancers(applyMiddleware(thunk)),
 );
 
