7ab7b4ce74e9263380a27edc762438fc1fdbce62
mateusz-pocztowski <55945204+mateusz-pocztowski@users.noreply.github.com>
2020-07-03 15:55:41 +0200

    Made changes for deployment

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,15 @@
-/* eslint-disable no-underscore-dangle */
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from 'reducers';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer, { initialState } from 'reducers';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistedState = localStorage.getItem('favnoteState')
+  ? JSON.parse(localStorage.getItem('favnoteState'))
+  : initialState;
 
-const persistedState = localStorage.getItem('reduxState')
-  ? JSON.parse(localStorage.getItem('reduxState'))
-  : {
-      userJWT: null,
-      userName: null,
-      userID: null,
-      isLoading: true,
-      isSubmitting: false,
-      error: {
-        status: null,
-        errorID: null,
-      },
-      modal: {
-        itemType: null,
-        itemID: null,
-      },
-    };
-
-const store = createStore(
-  rootReducer,
-  persistedState,
-  composeEnhancers(applyMiddleware(thunk)),
-);
+const store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
 
 store.subscribe(() => {
-  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+  localStorage.setItem('favnoteState', JSON.stringify(store.getState()));
 });
 
 export default store;
